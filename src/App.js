fb755f3d0cadc8c2d4d1cd16d781b9b9083b3e9d
Tsuki <kanin.twcr@gmail.com>
2021-11-06 13:07:47 +0700

    38. Adding normal js logic to component.
    - Add date convert logic
    - Fix amount bug
    - Change title component from div -> h2

--- src/App.js
+++ src/App.js
@@ -25,15 +25,31 @@ function App() {
       title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 11),
-    }
+    },
   ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amout} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amout} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amout} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amout} date={expenses[3].date}></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,19 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-Us', { month: 'long' });
+  const day = props.date.toLocaleString('en-Us', { month: '2-digit' });
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <dic>{props.date.toISOString()}</dic>
+      <dic>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </dic>
       <div className="expense-item__description">
-        <div>{props.title}</div>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
