47c8cb756e0b64bcbce86d296ff72302b7b9c88b
Tsuki <kanin.twcr@gmail.com>
2021-11-06 12:52:12 +0700

    37. Passing data via 'props'
    Create data on App.js and passing to ExpenseItem.js via props (properties)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2,28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <dic>{expenseDate.toISOString()}</dic>
+      <dic>{props.date.toISOString()}</dic>
       <div className="expense-item__description">
-        <div>{expenseTitle}</div>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <div>{props.title}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
