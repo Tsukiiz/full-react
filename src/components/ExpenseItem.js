ab1f7643eef42d904736407ef44e1638498e49ef
Tsuki <kanin.twcr@gmail.com>
2021-11-06 13:23:09 +0700

    39. Splitting components into multiple components
    - Split Calendar section into ExpenseDate.js
    - Adding ExpenseDate.css
    - Import ExpenseDate.js into ExpenseItem.js and passing the data over its.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,12 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-Us', { month: 'long' });
-  const day = props.date.toLocaleString('en-Us', { month: '2-digit' });
-  const year = props.date.getFullYear();
+  
 
   return (
     <div className="expense-item">
-      <dic>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </dic>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
